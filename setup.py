371660d9c573cc37080bf3792adfda49268768e9
varunsardana <varunsardana2006@gmail.com>
2025-08-02 14:59:37 -0700

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Varun",
+    author_email="varunsardana2006@gmail.com",
+    packages=find_packages()
+)
